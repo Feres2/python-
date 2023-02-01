9c97d077558ef1b42616cbae202062bdab8c6b86
Feres2 <feresalyani@gmail.com>
2023-02-01 13:36:37 +0100

    hello world

---
+++ fundamentales/introduction/hello_world.py
@@ -0,0 +1,17 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( "hello",name,"!" )	# with a comma
+print( "hello"+name+"!" )	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 42
+print( "hello",42,"!")	# with a comma
+print(  "hello" + str(42)+"!")	# with a +	-- this one should give us an error!
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(f"my favorite foods are {fave_food1} and {fave_food2}")
+
+
+
